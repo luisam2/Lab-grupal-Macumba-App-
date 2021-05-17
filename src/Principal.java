7bb875071c1307909f66155e0f9f166f8add5534
Luisa Marinez <luisa.marinez1@correo.icesi.edu.co>
2021-05-16 22:25:33 -0500

    datos

---
+++ src/Principal.java
@@ -0,0 +1,29 @@
+/***import java.io.Console;
+import java.io.File;
+import java.util.ArrayList;****/
+
+import processing.core.PApplet;
+
+public class Principal extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("Principal");
+	}
+
+	@Override
+	public void settings() {
+		size(500, 500);
+	}
+	
+	
+	@Override
+	public void setup() {
+		
+	}
+	
+	@Override
+	public void draw() {
+		background(255);
+			
+	}
+}
