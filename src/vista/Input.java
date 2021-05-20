ba5a8cb3f867890339550ecf4092ad9f51f7b7b9
Ingrid Vallecilla <stepha4424@gmail.com>
2021-05-19 22:16:42 -0500

    prueba con Input clase propia

--- src/vista/Input.java
+++ src/vista/Input.java
@@ -1,5 +1,68 @@
 package vista;
 
-public class Input {
+import controlP5.ControlP5;
+import controlP5.Textfield;
+import processing.core.PApplet;
+import processing.core.PFont;
 
+public class Input {
+	
+	private ControlP5 cp5;
+	private PApplet app;
+	private int tipo;
+	private int posX;
+	private int posY;
+	private int tamX;
+	private int tamY;
+	private PFont font;
+	private String textValue;
+	
+	public Input(ControlP5 cp5, PApplet app, int tipo, int posX, int posY, int tamX, int tamY) {
+		super();
+		this.cp5 = cp5;
+		this.app = app;
+		this.tipo = tipo;
+		this.posX = posX;
+		this.posY = posY;
+		this.tamX = tamX;
+		this.tamY = tamY;
+		this.textValue = "funciona";
+		
+		this.font = this.app.createFont("./font/gorditaregular.otf", 20);
+		
+		this.cp5.addTextfield("input")
+		.setPosition(this.posX, this.posY)
+		.setSize(this.tamX, this.tamY)
+		.setFont(this.font)
+		.setColor(this.app.color(255,0,0));
+		
+		  this.cp5.addTextfield("textValue")
+		     .setPosition(this.posX, this.posY)
+		     .setSize(this.tamX, this.tamY)
+		     .setFont(this.font)
+		     .setAutoClear(false)
+		     ;
+		       
+		  this.cp5.addBang("clear")
+		     .setPosition(this.posX, this.posY)
+		     .setSize(this.tamX, this.tamY)
+		     .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
+		     ;    
+		  
+		  this.cp5.addTextfield("default")
+		     .setPosition(this.posX, this.posY)
+		     .setAutoClear(false)
+		     ;
+		     
+		
+		this.app.textFont(font);
+	}
+
+	public void pintar() {
+		this.app.fill(255);
+		this.app.text(this.cp5.get(Textfield.class,"input").getText(), 360,130);
+		 this.app.text(textValue, 360,180);
+	}
+	
+	
 }
--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -6,28 +6,32 @@ import java.util.ArrayList;****/
 import processing.core.PApplet;
 import controlP5.*;
 
-public class Principal extends PApplet{
+public class Principal extends PApplet {
 
 	public static void main(String[] args) {
 		PApplet.main(Principal.class.getName());
 	}
 	
 	ControlP5 cp5;
+	Input test;
 
 	@Override
 	public void settings() {
 		size(375, 812);
+		
 	}
 	
 	
 	@Override
 	public void setup() {
-		
+		cp5 = new ControlP5(this);
+		test = new Input(cp5, this, 0, 200, 200, 100, 50);
 	}
 	
 	@Override
 	public void draw() {
 		background(255);
-			
+		test.pintar();
+		text("sdfd",50,50);
 	}
 }
