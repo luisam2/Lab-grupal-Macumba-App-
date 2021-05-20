9e169458c209664185d31fb8a9f0c93b12b50fc7
Ingrid Vallecilla <stepha4424@gmail.com>
2021-05-19 21:11:54 -0500

    agregar libreria controlP5

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -1,18 +1,22 @@
+package vista;
 /***import java.io.Console;
 import java.io.File;
 import java.util.ArrayList;****/
 
 import processing.core.PApplet;
+import controlP5.*;
 
 public class Principal extends PApplet{
 
 	public static void main(String[] args) {
-		PApplet.main("Principal");
+		PApplet.main(Principal.class.getName());
 	}
+	
+	ControlP5 cp5;
 
 	@Override
 	public void settings() {
-		size(500, 500);
+		size(375, 812);
 	}
 	
 	
