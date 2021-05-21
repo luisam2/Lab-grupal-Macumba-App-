669d32f5db136ff7e20b123272661fe85ce2612e
Luisa Marinez <luisa.marinez1@correo.icesi.edu.co>
2021-05-21 13:33:37 -0500

    crear comidas disponibles

--- src/modelo/Logica.java
+++ src/modelo/Logica.java
@@ -5,10 +5,18 @@ import java.util.ArrayList;
 public class Logica {
 
 	ArrayList<Usuario> usuarios;
+	ArrayList<Producto> productos;
 
 	public Logica() {
 		super();
 		usuarios = new ArrayList<>();
+		productos = new ArrayList<>();
+		
+		productos.add(new Producto("Ceviche", "30", 0, ""));
+		productos.add(new Producto("Arroz atollado", "25", 1, ""));
+		productos.add(new Producto("Casuela de marriscos", "40", 2, ""));
+		productos.add(new Producto("Empanadas de camaron", "10", 3, ""));
+		
 	}
 	
 	public void addUser(String usuario, String contraseña) {
